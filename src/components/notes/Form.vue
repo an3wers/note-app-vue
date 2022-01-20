853e0bb8320830d22022c5ebea656d6fb67feafd
an3wers <an3wers@gmail.com>
2022-01-20 16:52:24 +0500

    init form component

---
+++ src/components/notes/Form.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <textarea
+      class="form-control mb-3"
+      type="text"
+      required
+      v-model="value"
+      placeholder="Type your note"
+    />
+    <button class="btn" type="submit">Add new note</button>
+  </form>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      this.$emit('onSubmit', this.value)
+      this.value = ''
+    }
+  }
+}
+</script>
+
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>Home page</h1>
+  
+    <h1 class="mb-3 ">Home page</h1>
+
+    <Form @onSubmit="handleSubmit" />
+  
 </template>
+
+
+<script>
+import Form from '@/components/notes/Form'
+
+export default {
+  components: {
+    Form
+  },
+
+  methods: {
+      handleSubmit(value) {
+          console.log(value)
+      }
+  }
+}
+</script>
